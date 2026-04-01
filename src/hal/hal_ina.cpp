d447b757f58d149a1480c2cff08a79140c449875
BaiHengRui <bhr13151022@163.com>
2026-04-01 22:06:31 +0800

    优化INA228初始化注释，明确温度系数材料为铜锰合金

--- src/hal/hal_ina.cpp
+++ src/hal/hal_ina.cpp
@@ -21,7 +21,7 @@ bool HAL::INA22x_Init(){
     ina.calibrate(0.005f,8,ADC_RANGE_40_96mV); //5m ohm 最大期望电流8A 40.96mV量程(max 8A * 5mOhm = 40mV)
     #if INA228_EN
     //INA228特有功能，设置温度系数和启用温度补偿
-        ina.setShuntTemperatureCoefficient(100); // 设置温度系数100ppm/*C
+        ina.setShuntTemperatureCoefficient(100); //铜锰合金电阻 温度系数100ppm/*C
         ina.enableTemperatureCompensation(true);
     #endif
     HAL::INA22x_SetConfig(HAL::Sys_NVS_Valid("sample_mode", sample_mode, 2));
